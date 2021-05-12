6dce7a6e39a7f0d4b84adaeac223782fd0881b09
victorlikhachov <antos2512@hotmail.com>
2021-05-12 16:27:31 -0400

    1 file modified

--- dicontainer.ts
+++ dicontainer.ts
@@ -3,9 +3,12 @@ import 'reflect-metadata';
 import { Ninja } from './ninja';
 import { TYPES } from './types';
 import { IWarrior } from './warrior';
+import getDecorators from 'inversify-inject-decorators';
 
 export var container = new Container();
 
 container.bind<IWarrior>(TYPES.Warrior).to(Ninja);
 
 const { lazyInject } = getDecorators(container, false);
+
+export { lazyInject };
