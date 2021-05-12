654c2c9b7a02c5cd8a97c8f54e13a2429ec4888c
victorlikhachov <antos2512@hotmail.com>
2021-05-12 16:50:36 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -6,24 +6,29 @@ import { TYPES } from './types';
 import { IWeapon } from './iweapon';
 
 class WarriorFactory {
-  @lazyInject(TYPES.Warrior)
-  warrior: IWarrior;
+  public getWarriorByName(name: string): IWarrior {
+    return new NamedWarrior(name);
+  }
 }
 
 class NamedWarrior implements IWarrior {
   @lazyInject(TYPES.Weapon)
   public weapon: IWeapon;
-  public constructor(public name:string){}
-  fight: () => string;
-  sneak: () => string;
-;
+  public constructor(public name: string) {}
+  public fight() {
+    return `My name: '${this.name}';  I do: '${this.weapon.hit()}''`;
+  }
+  public sneak() {
+    return 'sneak';
+  }
 }
 
 var ninja = container.get<IWarrior>(TYPES.Warrior);
 const fight = ninja.fight();
 
-const a = new WarriorFactory();
-const s = a.warrior.fight();
+const factory = new WarriorFactory();
+const namedWarrior = factory.getWarriorByName('Victor');
+const namedWarriorCry = namedWarrior.fight();
 
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Ninja says: ${fight}</h1>`;
+appDiv.innerHTML = `<h1>Ninja says: ${namedWarriorCry}</h1>`;
