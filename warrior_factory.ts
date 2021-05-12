71f0c9afa5ae99ae0f28c143f4a8738eaf720f45
victorlikhachov <antos2512@hotmail.com>
2021-05-12 17:31:49 -0400

    1 file modified

--- warrior_factory.ts
+++ warrior_factory.ts
@@ -1,11 +1,15 @@
-import { injectable } from 'inversify';
+import { Container, injectable } from 'inversify';
 import { IWarriorFactory } from './iwarriorfactory';
+import { IWeapon } from './iweapon';
 import { NamedWarrior } from './named-warrior';
+import { TYPES } from './types';
 import { IWarrior } from './warrior';
 
 @injectable()
 export class WarriorFactory implements IWarriorFactory {
+  public constructor (private container: Container){};
   public getWarriorByName(name: string): IWarrior {
-    return new NamedWarrior(name);
+    var weapon = this.container.get<IWeapon>(TYPES.Weapon);
+    return new NamedWarrior(name,weapon);
   }
 }
