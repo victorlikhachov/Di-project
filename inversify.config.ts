73d64eca43223e2881b36c7dc9c4c4536db84fec
victorlikhachov <antos2512@hotmail.com>
2021-05-14 11:10:48 -0400

    1 file modified

--- inversify.config.ts
+++ inversify.config.ts
@@ -5,7 +5,6 @@ import { TYPES } from './types';
 import { IWarrior } from './warrior';
 import getDecorators from 'inversify-inject-decorators';
 import { IWeapon } from './iweapon';
-import { Katana } from './katana';
 import { IWarriorFactory } from './iwarriorfactory';
 import { WarriorFactory } from './warrior_factory';
 import { NamedWarrior } from './named-warrior';
@@ -13,7 +12,6 @@ import { weapons_module } from './weapons-module';
 
 export var container = new Container();
 
-//container.bind<IWeapon>(TYPES.Weapon).to(Katana);
 container.bind<IWarrior>(TYPES.Warrior).to(Ninja);
 container.bind<IWarriorFactory>(TYPES.WarriorFactory).to(WarriorFactory);
 
